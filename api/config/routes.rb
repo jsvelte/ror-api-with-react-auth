12b7e4f6ecd1162834274ecf00733e792d33d76f
jsvelte <joshua.galit@sun-asterisk.com>
2023-04-24 23:15:08 +0800

    [RAAR-004] - [BE] Implement Auth Sign In/Sign Up Using Devise Token Auth

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    resources :contacts
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: '/auth'
+      resources :articles
+    end
   end
 end
--- api/test/models/article_test.rb
+++ api/test/models/article_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class V::ContactsControllerTest < ActionDispatch::IntegrationTest
+class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
